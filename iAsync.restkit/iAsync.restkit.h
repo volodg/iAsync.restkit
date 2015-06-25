8012106ec7d43694261d538dd863b77d376a3ba2
volodg <volodg@mail.ru>
2015-06-25 21:04:40 +0300

    first commit

---
+++ iAsync.restkit/iAsync.restkit.h
@@ -0,0 +1,19 @@
+//
+//  iAsync.restkit.h
+//  iAsync.restkit
+//
+//  Created by Vladimir Gorbenko on 25.06.15.
+//  Copyright (c) 2015 EmbeddedSources. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for iAsync.restkit.
+FOUNDATION_EXPORT double iAsync_restkitVersionNumber;
+
+//! Project version string for iAsync.restkit.
+FOUNDATION_EXPORT const unsigned char iAsync_restkitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iAsync_restkit/PublicHeader.h>
+
+
