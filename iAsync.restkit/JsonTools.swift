209555de9d8c05a052343586d7da79c11eaeb959
vlg <gorbenko.vova@gmail.com>
2016-02-11 21:09:48 +0300

    refactor with using of reactiveKit

--- iAsync.restkit/JsonTools.swift
+++ iAsync.restkit/JsonTools.swift
@@ -13,9 +13,18 @@ import iAsync_reactiveKit
 
 import ReactiveKit
 
+public extension AsyncStreamType where Self.Value == NSData, Self.Error == NSError {
+
+    func toJson() -> AsyncStream<AnyObject, AnyObject, NSError> {
+
+        let stream = self.mapNext { _ in NSNull() as AnyObject }
+        return stream.flatMap { JsonTools.jsonLoader($0) }
+    }
+}
+
 public struct JsonTools {
 
-    public static func jsonLoader(data: NSData, context: CustomStringConvertible) -> AsyncStream<AnyObject, AnyObject, NSError> {
+    public static func jsonLoader(data: NSData, context: CustomStringConvertible? = nil) -> AsyncStream<AnyObject, AnyObject, NSError> {
 
         return asyncStreamWithJob { (progress: AnyObject -> Void) -> Result<AnyObject, NSError> in
 
@@ -23,7 +32,7 @@ public struct JsonTools {
                 let jsonObj = try NSJSONSerialization.JSONObjectWithData(data, options: [.AllowFragments])
                 return .Success(jsonObj)
             } catch let error as NSError {
-                let resError = ParseJsonDataError(data: data, jsonError: error, context: context)
+                let resError = ParseJsonDataError(data: data, jsonError: error, context: context ?? "")
                 return .Failure(resError)
             } catch {
                 return .Failure(Error(description: "unexpected system state 2"))
